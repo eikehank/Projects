64491c427daaa135200af067204d4af67a9cf68b
Eike Caina Hank <cainaeike@gmail.com>
2020-06-13 22:00:47 -0300

    trabalho salas da universidade

---
+++ Salas da Universidade/laboratorios/fisica.java
@@ -0,0 +1,10 @@
+package laboratorios;
+
+import salas.laboratorio;
+
+public class fisica extends laboratorio {
+	
+	public static String equipamentos () {
+		return "Laboratórios de fisica são equipados com ferramentas para qualquer tipo de experiência.";
+	}
+}
---
+++ Salas da Universidade/salas/salaConferencia.java
@@ -0,0 +1,17 @@
+package salas;
+
+public class salaConferencia{
+
+	public static String capacidade() {
+		return "Salas de conferencias tem capacidade maxima de 10 pessoas.";
+	}
+
+	public static String projetor() {
+		return "Cada sala de conferencia é equipado com um projetor para auxilar nas apresentações.";
+	}
+
+	public static String pc() {
+		return "Salas de conferencia são equipadas com nootbooks para auxilio nas reuniões.";
+	}
+
+}
